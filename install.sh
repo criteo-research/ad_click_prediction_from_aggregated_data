da1d293090f8fed0a32f5966d09e3bbf4ab72054
a.damato <a.damato@criteo.com>
2021-03-23 09:13:00 +0000

    Rename .venv to .agg_model_venv
    This avoids having collision naming with cluster_pack. it can happen that another venv is named ".venv" and this will overwrite it when cluster pack is packaging this env. By changing the name to 'agg_model_venv' we ensure to collide only with this repo env.
    
    Change-Id: I595221f876417e96b80a521faf73dce54752df2e

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-python3.6 -m venv .venv
-. .venv/bin/activate
+python3.6 -m venv .agg_model_venv
+. .agg_model_venv/bin/activate
 pip install -U pip
 pip install -e .
-register-kernel --venv .venv --name aggregate_models
+register-kernel --venv .agg_model_venv --name aggregate_models_env
