e83d7c7e5415ec73c1d991bc6323c71f3c4eb8af
j.rioufougeras <j.rioufougeras@criteo.com>
2021-03-31 14:51:17 +0000

    Adding logging library
    Change-Id: I2093e0cc8aaa40966d9c44e75a2cd300d912efbf

--- aggregated_models/__init__.py
+++ aggregated_models/__init__.py
@@ -1,1 +1,15 @@
-#
+import sys
+import logging
+from logging import StreamHandler
+
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.INFO)
+
+stdout_handler = StreamHandler(stream=sys.stdout)
+stdout_handler.setLevel(logging.INFO)
+
+logger_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+stdout_handler.setFormatter(logger_formatter)
+
+root_logger.addHandler(stdout_handler)
