e8a7d1274d04713f74c965a8a49453c72b937726
j.rioufougeras <j.rioufougeras@criteo.com>
2021-04-02 15:50:24 +0000

    Adding RDD test
    - Cleaning test code
    
    Change-Id: Icbc474a4b6fcb00705522e4c15063412eb0d5f16

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,10 @@
 import pytest
+import pandas as pd
+import getpass
 from thx.hadoop import spark_config_builder
+from aggregated_models.validation import MetricsComputer
+from aggregated_models.featuremappings import AggDataset
+import uuid
 
 
 @pytest.fixture(scope="session")
@@ -7,5 +12,33 @@ def spark_session():
     ss = spark_config_builder.create_local_spark_session(
         "test-session", sql_num_partitions=1, properties=[("spark.ui.enabled", "false")]
     )
+    tmp_fold = str(uuid.uuid4())
+
+    checkpoint_dir = f"/tmp/{getpass.getuser()}/load/{tmp_fold}/"
+    ss.sparkContext.setCheckpointDir(checkpoint_dir)
+
     yield ss
     ss.stop()
+
+
+class ModelTestData:
+    def __init__(self):
+        self.train = pd.read_parquet("./tests/resources/small_train.parquet")
+        self.valid = pd.read_parquet("./tests/resources/small_valid.parquet")
+
+        self.features = ["cat1", "cat4", "cat6", "cat8", "cat9"]
+        self.label = "click"
+
+        self.validator = MetricsComputer(self.label)
+
+        # parameters for of the privacy protecting noise.
+        self.epsilon = None  # Set to None to get no noise.
+        self.delta = None
+        self.aggdata = AggDataset(
+            self.features, "*&*", self.train, self.label, self.epsilon, self.delta, maxNbModalities=10000
+        )
+
+
+@pytest.fixture(scope="session")
+def model_data():
+    return ModelTestData()
---
+++ tests/test_rdd_mrf_model.py
@@ -0,0 +1,24 @@
+from pytest import approx
+from aggregated_models.agg_mrf_model import AggMRFModel
+
+
+def test_spark_agg_mrf_model(spark_session, model_data):
+    regulL2 = 16
+    nbSamples = 10000
+    nbIter = 50
+    memMrf = AggMRFModel(
+        model_data.aggdata,
+        model_data.features,
+        exactComputation=False,  # Using Gibbs Sampling.  actualy exact=True is broken in latest code
+        clicksCfs="*&*",  # crossfeatures used by P(Y|X) part of the model
+        displaysCfs="*&*",  # crossfeatures used by P(X) part of the model. Here, all pairs + all single .
+        nbSamples=nbSamples,  # Nb Gibbs samples to estimate gradient
+        regulL2=1.0,  # parmeter "lambda_2"
+        regulL2Click=regulL2,  # parmeter "lambda_1"
+        sampleFromPY0=True,
+        maxNbRowsperGibbsUpdate=100,
+        sparkSession=spark_session,
+    )
+    memMrf.fit(nbIter, 0.05)
+    assert model_data.validator.getLLH(memMrf, model_data.train) == approx(0.057, rel=0.1)
+    assert model_data.validator.getLLH(memMrf, model_data.valid) == approx(0.057, rel=0.1)
