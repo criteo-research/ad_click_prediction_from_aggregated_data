4c43e00fe0a4493ee86a8060af3c8571c92d0394
j.rioufougeras <j.rioufougeras@criteo.com>
2021-04-02 15:50:25 +0000

    Add requirements to install script
    Change-Id: Icc1bad9e781c288ccb51ee4f5ec588470a4e0503

--- install.sh
+++ install.sh
@@ -4,4 +4,6 @@ python3.6 -m venv .agg_model_venv
 . .agg_model_venv/bin/activate
 pip install -U pip
 pip install -e .
+pip install -r dev-requirements.txt
+pip install -r tests-requirements.txt
 register-kernel --venv .agg_model_venv --name aggregate_models_env
